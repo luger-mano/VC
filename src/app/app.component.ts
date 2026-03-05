bd0695f32c4ec8f511e881c282dda32fb3c3e9fa
luger-mano <lucas.germanysantos@gmail.com>
2026-03-04 23:54:50 -0300

    [refact] : Adicionei novas funcionalidade para a tela, menu hamburguer com opções, favoritar produto, header com efeitos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AfterViewInit, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 
 @Component({
@@ -7,8 +9,89 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  hideHeader = false;
+  headerBackground = false;
+  lastScrollY = 0;
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+
+  const currentScroll = window.scrollY;
+
+  if (currentScroll > this.lastScrollY && currentScroll > 80) {
+    // scroll para baixo
+    this.hideHeader = true;
+  } else {
+    // scroll para cima
+    this.hideHeader = false;
+
+    if (currentScroll > 0) {
+      this.headerBackground = true;
+    }
+  }
+
+  // remove fundo quando voltar ao topo
+  if (currentScroll === 0) {
+    this.headerBackground = false;
+  }
+
+  this.lastScrollY = currentScroll;
+}
+
+  menuOpen = false;
+
+  openMenu() {
+    this.menuOpen = true;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.style.overflow = 'auto';
+  }
+
+  likedIndex: number | null = null;
+
+  likeProduct(index: number) {
+    this.likedIndex = index;
+
+    setTimeout(() => {
+      this.likedIndex = null;
+    }, 800);
+  }
+  cartCount = 0;
+
+  addToCart() {
+    this.cartCount++;
+  }
   title = 'Vovó Crochê';
+
+  @ViewChildren('marqueeRef')
+  marquees!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+
+  this.marquees.forEach((marqueeEl: ElementRef) => {
+
+    const marquee = marqueeEl.nativeElement;
+    let position = -marquee.offsetWidth;
+
+    const animate = () => {
+      position += 3.8;
+
+      if (position >= window.innerWidth) {
+        position = -marquee.offsetWidth;
+      }
+
+      marquee.style.transform = `translateX(${position}px)`;
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  });
+
+}
 }
